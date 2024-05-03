70a4b4a6f777161c987d60fbc2295815d5c9ebbb
Stoyan Kirov <stoian.kiroff@gmail.com>
2024-05-03 20:01:20 +0300

    Add base logic for verification

--- ampd/src/starknet/verifier.rs
+++ ampd/src/starknet/verifier.rs
@@ -8,6 +8,7 @@ use url::Url;
 use super::events::contract_call::ContractCallEvent;
 use super::json_rpc::{Client, StarknetClientError};
 use crate::handlers::starknet_verify_msg::Message;
+use crate::starknet::json_rpc::StarknetClient;
 
 #[derive(Error, Debug)]
 pub enum VerifierError {
@@ -42,12 +43,34 @@ impl MessageVerifier for RPCMessageVerifier {
     /// Verify that a tx with a certain `tx_hash` has happened on Starknet.
     /// `tx_hash` comes from the the Axelar `Message::tx_id`
     async fn verify_msg(&self, msg: &Message) -> core::result::Result<Vote, VerifierError> {
-        unimplemented!()
+        match self
+            .client
+            .get_event_by_hash(msg.tx_id.as_str())
+            .await
+            .map_err(VerifierError::FetchEvent)?
+        {
+            Some((event_tx_hash, contract_call_event)) => {
+                println!("MESSAGE {:?}", msg);
+                println!("CONTRACT_CALL_EVENT {:?}", contract_call_event);
+                println!("EVENT_TX_HASH {:?}", event_tx_hash);
+                if event_tx_hash == msg.tx_id && contract_call_event == msg
+                //     && event.type_ == EventType::ContractCall.struct_tag(gateway_address)
+                {
+                    Ok(Vote::SucceededOnChain)
+                } else {
+                    Ok(Vote::FailedOnChain)
+                }
+            }
+            None => Ok(Vote::NotFound),
+        }
     }
 }
 
 impl PartialEq<&Message> for ContractCallEvent {
     fn eq(&self, axl_msg: &&Message) -> bool {
-        unimplemented!()
+        axl_msg.source_address == self.source_address
+            && axl_msg.destination_chain == self.destination_chain
+            && axl_msg.destination_address == self.destination_address
+            && axl_msg.payload_hash == self.payload_hash
     }
 }
