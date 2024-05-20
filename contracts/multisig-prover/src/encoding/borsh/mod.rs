975d81e6a6042407189a41fcf0ee1153282eca4d
tilacog <tilacog@gmail.com>
2024-05-20 15:15:14 -0300

    feat(multisig-prover): implement borsh::make_operators function

--- contracts/multisig-prover/src/encoding/borsh/mod.rs
+++ contracts/multisig-prover/src/encoding/borsh/mod.rs
@@ -4,8 +4,18 @@ use multisig::worker_set::WorkerSet;
 
 pub mod execute_data;
 
-pub fn make_operators(_worker_set: WorkerSet) -> Operators {
-    todo!()
+pub fn make_operators(worker_set: WorkerSet) -> Operators {
+    use multisig::key::PublicKey::*;
+    let mut weights_by_address = Vec::with_capacity(worker_set.signers.len());
+    for signer in worker_set.signers.into_values() {
+        let (Ecdsa(pubkey) | Ed25519(pubkey)) = signer.pub_key;
+        weights_by_address.push((pubkey, signer.weight))
+    }
+    Operators::new(
+        weights_by_address,
+        worker_set.threshold,
+        worker_set.created_at,
+    )
 }
 
 pub fn payload_hash_to_sign(
