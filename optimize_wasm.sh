1d0aa14e1391d21152c1836b7b6a3fad52bee739
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-11-07 10:21:47 +0200

    add shortcat to optimize wasm binaries

---
+++ optimize_wasm.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+TARGET=$1
+TARGET="${TARGET/-/_}"
+MY_DIR="$(dirname "$(realpath "$0")")"
+OUTPUT_DIR="$MY_DIR/artifacts"
+
+if ! which wasm-opt >/dev/null 2>&1; then
+    echo "Error: wasm-opt is not installed. Please install it and try again."
+    echo "Error: Check this repo https://github.com/WebAssembly/binaryen for wasm-opt"
+    exit 1
+fi
+
+mkdir -p "$OUTPUT_DIR"
+wasm-opt -Oz -o "$OUTPUT_DIR/$TARGET.wasm" "$MY_DIR/target/wasm32-unknown-unknown/release/$TARGET.wasm"
