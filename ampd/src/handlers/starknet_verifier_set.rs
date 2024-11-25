cfc0a65e0c57ce941de81c1dc227f6f02485c357
Marcin Bieszke <marcinbieszke@gmail.com>
2024-11-25 14:33:11 +0100

    save what i have before go

---
+++ ampd/src/handlers/starknet_verifier_set.rs
@@ -0,0 +1,54 @@
+use std::convert::TryInto;
+
+use async_trait::async_trait;
+use axelar_wasm_std::voting::{PollId, Vote};
+use cosmrs::cosmwasm::MsgExecuteContract;
+use cosmrs::tx::Msg;
+use cosmrs::Any;
+use error_stack::ResultExt;
+use events::Error::EventTypeMismatch;
+use events_derive::try_from;
+use multisig::verifier_set::VerifierSet;
+use serde::Deserialize;
+use starknet_core::types::FieldElement;
+use tokio::sync::watch::Receiver;
+use tracing::{info, info_span};
+use valuable::Valuable;
+use voting_verifier::msg::ExecuteMsg;
+
+use crate::event_processor::EventHandler;
+use crate::handlers::errors::Error;
+use crate::starknet::json_rpc::StarknetClient;
+use crate::starknet::verifier::verify_verifier_set;
+use crate::types::{Hash, TMAddress};
+
+/// Module responsible for handling verification of verifier set changes on Starknet.
+/// It processes events related to verifier set updates, verifies them against the Starknet chain,
+/// and manages the voting process for confirming these changes.
+
+#[derive(Deserialize, Debug)]
+pub struct VerifierSetConfirmation {
+    pub tx_hash: FieldElement,
+    pub event_index: u32,
+    pub verifier_set: VerifierSet,
+}
+
+#[derive(Deserialize, Debug)]
+#[try_from("wasm-verifier_set_poll_started")]
+struct PollStartedEvent {
+    poll_id: PollId,
+    source_gateway_address: String,
+    verifier_set: VerifierSetConfirmation,
+    participants: Vec<TMAddress>,
+    expires_at: u64,
+}
+
+pub struct Handler<C>
+where
+    C: StarknetClient + Send + Sync,
+{
+    verifier: TMAddress,
+    voting_verifier_contract: TMAddress,
+    blockchain: C,
+    latest_block_height: Receiver<u64>,
+}
