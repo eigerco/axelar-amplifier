3a5e951d437f249c0be0bbc30797987cd8c39800
Angelos Stylianidis <agathangelos.stylianidis@gmail.com>
2024-12-10 12:51:19 +0200

    multisig-aleo

---
+++ contracts/multisig-aleo/src/lib.rs
@@ -0,0 +1,23 @@
+mod client;
+pub use client::Client;
+pub mod contract;
+pub mod error;
+pub mod events;
+pub mod key;
+pub mod msg;
+pub mod multisig;
+mod signing;
+mod state;
+pub mod types;
+pub mod verifier_set;
+
+#[cfg(feature = "secp256k1")]
+mod secp256k1;
+
+#[cfg(feature = "ed25519")]
+mod ed25519;
+
+#[cfg(any(test, feature = "test"))]
+pub mod test;
+
+pub use crate::error::ContractError;
