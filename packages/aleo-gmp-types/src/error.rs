cf60c83774a6548cc033dad818e3f1f2d4289dec
anstylian <11269403+anstylian@users.noreply.github.com>
2025-08-13 21:43:28 +0300

    feat: Aleo its translation (#78)
    * Translating message from and to aleo for ITS-hub

---
+++ packages/aleo-gmp-types/src/error.rs
@@ -0,0 +1,13 @@
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum Error {
+    #[error(transparent)]
+    StringEncoderError(#[from] aleo_string_encoder::AleoStringEncoderError),
+    #[error(transparent)]
+    RouterApi(#[from] router_api::error::Error),
+    #[error("Invalid chain name: {0}")]
+    InvalidChainName(String),
+    #[error(transparent)]
+    AxelarInvalidChainName(#[from] axelar_wasm_std::chain::Error),
+}
